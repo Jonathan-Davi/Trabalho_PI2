62f8b648d94544820e9676975775def076b83f80
Jonathan Davi <jonathanufu@hotmail.com>
2024-10-08 11:16:07 -0300

    bg

---
+++ usuario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/usuario.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>FitTrack Usuário</title>
+</head>
+<body>
+    
+    <main>
+
+        <div class="sec-main">
+            <div class="main-foto">
+
+            </div>
+            <div class="main-arquivo">
+
+            </div>
+            <div class="main-esquerda">
+                <div class="esquerda-infos">
+                    <div class="infos-titulo">
+                        <p>Informações</p>
+                    </div>
+                    <div class="infos">
+
+                    </div>
+                </div>
+            </div>
+            <div class="main-direita">
+
+            </div>
+        </div>
+
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+</body>
+</html>
