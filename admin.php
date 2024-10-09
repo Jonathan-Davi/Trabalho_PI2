fab84bc050299049bb9228824f178f8df2a637e1
Jonathan Davi <jonathanufu@hotmail.com>
2024-10-09 10:59:41 -0300

    at

---
+++ admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="admin.css">
+    
+    <title>FitTrack Admin</title>
+</head>
+<body>
+    
+    <main>
+
+        <div class="container-main">
+            <div class="main-esquerda">
+                <div class="esquerda-foto">
+                    <img src="" alt="">
+                </div>
+                <div class="esquerda-infos">
+
+                </div>
+            </div>
+            <div class="main-direita">
+
+            </div>
+        </div>
+
+    </main>
+
+</body>
+</html>
